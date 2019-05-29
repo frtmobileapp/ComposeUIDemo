94ae794f4456e733513ca93310eb104a74cc172a
WJX <1065351931@qq.com>
2019-05-29 19:11:37 +0800

    wjx

--- app/src/main/java/com/nd/frt/recentconversation/model/UserInfo.java
+++ app/src/main/java/com/nd/frt/recentconversation/model/UserInfo.java
@@ -1,9 +1,15 @@
 package com.nd.frt.recentconversation.model;
 
+import java.io.Serializable;
+
 public class UserInfo {
 
+public class UserUnfo implements Serializable{
     public String avatarUrl;
-    public String userName;
-    public String content;
+    public String uesrName;
+
+}
+
+
 
 }
