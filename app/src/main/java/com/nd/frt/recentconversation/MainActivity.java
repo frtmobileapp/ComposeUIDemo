12c507d24253c6fa70ab94e0237fb3552475bbea
koala0206 <1151136628@qq.com>
2019-06-06 13:48:41 +0800

    asd

--- app/src/main/java/com/nd/frt/recentconversation/MainActivity.java
+++ app/src/main/java/com/nd/frt/recentconversation/MainActivity.java
@@ -1,17 +1,22 @@
 package com.nd.frt.recentconversation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.nd.frt.recentconversation.adapter.UserAdapter;
 import com.nd.frt.recentconversation.model.UserInfo;
 import com.nd.frt.recentconversation.service.UserInfoService;
 
 import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
+
+    private UserAdapter mUserAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +24,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         UserInfoService userInfoService = new UserInfoService();
         List<UserInfo> userInfos = userInfoService.getUserInfos(this);
-        UserAdapter userAdapter = new UserAdapter(userInfos);
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvRecyclerView);
+        mUserAdapter = new UserAdapter(userInfos);
+        RecyclerView recyclerView = findViewById(R.id.rvRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        recyclerView.setAdapter(userAdapter);
+        recyclerView.setAdapter(mUserAdapter);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_add) {
+            UserInfo userInfo = new UserInfo();
+            userInfo.userName = "eyyong";
+            userInfo.content = "kongbai@126.com";
+            userInfo.avatarUrl = "https://randomuser.me/api/portraits/women/64.jpg";
+            mUserAdapter.add(userInfo);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_OK) {
+            if (data != null) {
+                UserInfo userInfo = (UserInfo) data.getSerializableExtra(DetailActivity.PARAM_USER_INFO);
+                int index = data.getIntExtra(DetailActivity.PARAM_USER_INDEX, 0);
+                mUserAdapter.edit(index, userInfo);
+            }
+        }
+    }
 }
